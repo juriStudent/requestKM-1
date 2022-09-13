428d55f287635063162e90f1020d0731521c5c0e
nNoidea <58337790+nNoidea@users.noreply.github.com>
2022-09-13 20:23:12 +0200

    init

---
+++ index.js
@@ -0,0 +1,33 @@
+"use strict";
+function clickedSubmit(input) {
+    const box = document.getElementById('userMessage');
+    let km = Number(input);
+    let urlArray = (window.location.search).split("?");
+    let name = urlArray[1];
+    let vehicleCode = urlArray[2];
+    let lastKM = Number(urlArray[3]);
+    let sessionID = urlArray[4];
+    // Input is not a number.
+    if (isNaN(km)) {
+        if (box != null) {
+            box.style.color = 'red';
+            box.textContent = "Typ alleen cijfers.";
+        }
+        return;
+    }
+    // is equal allowed???????????????????????????????
+    if (km <= lastKM) {
+        if (box != null) {
+            box.style.color = "red";
+            box.textContent = `Uw km moet groter zijn dan je laatste km: ${lastKM}km`;
+        }
+        return;
+    }
+    alert(km);
+}
+function submit(km, sessionID) {
+    let url = `https://google.com/${Number}/${sessionID}/asohudoasdasdasd`;
+    window.location.replace(url);
+}
+//?{Name}?{vehicleCode}?{km}?{sessionID}
+//?Cedric?ABC123?12312312?XYZ123
