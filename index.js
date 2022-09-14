c35c9a6abebaa4c6e2332a8ff954d808a450901b
juriStudent <113450135+juriStudent@users.noreply.github.com>
2022-09-14 14:30:45 +0200

    - added extra params to show an error or ok page. - allowed for enter to be used.

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+        clickedSubmit(document.getElementById('km').value)
+    }
+});
+
 "use strict";
 function clickedSubmit(input) {
     const box = document.getElementById('userMessage');
@@ -7,6 +13,9 @@ function clickedSubmit(input) {
     let vehicleCode = urlArray[2];
     let lastKM = Number(urlArray[3]);
     let sessionID = urlArray[4];
+
+
+
     // Input is not a number.
     if (isNaN(km)) {
         if (box != null) {
@@ -26,8 +35,31 @@ function clickedSubmit(input) {
     submit(workerName, vehicleCode, km, sessionID);
 }
 function submit(workerName, vehicleCode, km, sessionID) {
-    let url = `http://localhost:7071/api/${workerName}/${vehicleCode}/${km}/${sessionID}`;
+    // Obviously half link
+    let url = `.azurewebsites.net/api/${workerName}/${vehicleCode}/${km}/${sessionID}?code=`
+    // let url = `http://localhost:7071/api/${workerName}/${vehicleCode}/${km}/${sessionID}`;
     window.location.replace(url);
 }
 //?{Name}?{vehicleCode}?{km}?{sessionID}
 //?Cedric?ABC123?12312312?XYZ123
+
+window.onload = function pageLoad() {
+    const box = document.getElementById('userMessage');
+    let urlArray = (window.location.search).split("?");
+    let lastKM = Number(urlArray[3]);
+
+    if (urlArray[5] == "low") {
+        box.style.color = "red";
+        box.textContent = `Uw km moet groter zijn dan je laatste km: ${lastKM}km`;
+    }
+    else if (urlArray[5] == "ok") {
+        box.style.color = "green";
+        box.textContent = `Afgelegde weg verandert naar: ${lastKM}km`;
+
+        // Hide the text input box.
+        document.getElementById('km').style.display = "none";
+
+        // Hide the submit button.
+        document.getElementById("submitButton").style.display = "none";
+    }
+}
